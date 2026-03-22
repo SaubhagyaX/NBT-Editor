6345ef58bd7b2acba58801870122a01844ed4378
SaubhagyaX <saubhagyakumar122@gmail.com>
2026-03-22 10:59:57 +0000

    Fix ChatScreenMixin keyPressed require=0

--- src/main/java/com/luneruniverse/minecraft/mod/nbteditor/mixin/ChatScreenMixin.java
+++ src/main/java/com/luneruniverse/minecraft/mod/nbteditor/mixin/ChatScreenMixin.java
@@ -37,7 +37,7 @@ public class ChatScreenMixin {
 		MixinLink.renderChatLimitWarning((ChatScreen) (Object) this, matrices);
 	}
 	
-	@Inject(method = "keyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;setScreen(Lnet/minecraft/client/gui/screen/Screen;)V"), cancellable = true)
+	@Inject(method = "keyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;setScreen(Lnet/minecraft/client/gui/screen/Screen;)V"), cancellable = true, require = 0)
 	private void keyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> info) {
 		if (!(MainUtil.client.currentScreen instanceof ChatScreen)) {
 			info.setReturnValue(true);
